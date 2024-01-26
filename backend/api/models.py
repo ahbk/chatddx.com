0fa9b1b0d1d71b4a94d34c1fcfe2ec11bfd6b02a
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-01-26 20:02:55 +0100

    ruff

--- backend/api/models.py
+++ backend/api/models.py
@@ -11,6 +11,7 @@ class OpenAIMessageRole(models.Model):
 
     name = models.CharField(max_length=255)
 
+
 class OpenAIModel(models.Model):
     class Meta:
         verbose_name_plural = "OpenAI Models"
@@ -19,6 +20,7 @@ class OpenAIModel(models.Model):
         return self.name
 
     name = models.CharField(max_length=255)
+
 
 class OpenAILogitBias(models.Model):
     class Meta:
@@ -29,16 +31,18 @@ class OpenAILogitBias(models.Model):
 
     token = models.CharField(max_length=255)
     bias = models.IntegerField(
-            validators=[
-                MaxValueValidator(100),
-                MinValueValidator(-100),
-                ])
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(-100),
+        ]
+    )
 
     def serialize(self):
         return {
-                "token": self.token,
-                "bias": self.bias,
-                }
+            "token": self.token,
+            "bias": self.bias,
+        }
+
 
 class OpenAIMessage(models.Model):
     class Meta:
@@ -48,25 +52,25 @@ class OpenAIMessage(models.Model):
         return self.description
 
     description = models.CharField(
-            default="",
-            max_length=255,
-            )
+        default="",
+        max_length=255,
+    )
     content = models.TextField()
     role = models.ForeignKey(OpenAIMessageRole, on_delete=models.PROTECT)
     name = models.CharField(
-            max_length=255,
-            default=None,
-            null=True,
-            blank=True,
-            )
+        max_length=255,
+        default=None,
+        null=True,
+        blank=True,
+    )
 
     def serialize(self):
         m_dict = {
-                "role": self.role.name,
-                "content": self.content,
-                }
-        if not self.name is None:
-            m_dict['name'] = self.name
+            "role": self.role.name,
+            "content": self.content,
+        }
+        if self.name is not None:
+            m_dict["name"] = self.name
 
         return m_dict
 
@@ -79,9 +83,9 @@ class OpenAIChat(models.Model):
         return self.identifier
 
     identifier = models.CharField(
-            max_length=255,
-            unique=True,
-            )
+        max_length=255,
+        unique=True,
+    )
     active = models.BooleanField(default=True)
     endpoint = models.CharField(max_length=255)
     api_key = models.CharField(max_length=255)
@@ -89,89 +93,92 @@ class OpenAIChat(models.Model):
     model = models.ForeignKey(OpenAIModel, on_delete=models.PROTECT)
 
     frequency_penalty = models.FloatField(
-            default=None,
-            null=True,
-            blank=True,
-            validators=[
-                MinValueValidator(-2),
-                MaxValueValidator(2),
-                ])
+        default=None,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(-2),
+            MaxValueValidator(2),
+        ],
+    )
 
     logit_bias = models.ManyToManyField(
-            OpenAILogitBias,
-            blank=True,
-            )
+        OpenAILogitBias,
+        blank=True,
+    )
 
     max_tokens = models.IntegerField(
-            default=None,
-            null=True,
-            blank=True,
-            validators=[
-                MinValueValidator(1),
-                MaxValueValidator(2000),
-                ])
+        default=None,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(2000),
+        ],
+    )
 
     presence_penalty = models.FloatField(
-            default=None,
-            null=True,
-            blank=True,
-            validators=[
-                MinValueValidator(-2),
-                MaxValueValidator(2),
-                ])
+        default=None,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(-2),
+            MaxValueValidator(2),
+        ],
+    )
 
     temperature = models.FloatField(
-            default=None,
-            null=True,
-            blank=True,
-            validators=[
-                MinValueValidator(0),
-                MaxValueValidator(2),
-                ]
-            )
+        default=None,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(2),
+        ],
+    )
 
     top_p = models.FloatField(
-            default=None,
-            null=True,
-            blank=True,
-            validators=[
-                MinValueValidator(0),
-                MaxValueValidator(1),
-                ]
-            )
+        default=None,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(1),
+        ],
+    )
 
     user = models.CharField(
-            max_length=255,
-            default=None,
-            blank=True,
-            null=True,
-            )
+        max_length=255,
+        default=None,
+        blank=True,
+        null=True,
+    )
 
     def serialize(self):
         payload = {
-                "messages": [m.serialize() for m in self.messages.all()],
-                "model": self.model.name
-                }
+            "messages": [m.serialize() for m in self.messages.all()],
+            "model": self.model.name,
+        }
 
         logit_bias = self.logit_bias.all()
         if logit_bias:
-            payload['logit_bias'] = [lb.serialize() for lb in logit_bias]
+            payload["logit_bias"] = [lb.serialize() for lb in logit_bias]
 
         for attr in [
-                'frequency_penalty',
-                'max_tokens',
-                'presence_penalty',
-                'temperature',
-                'top_p',
-                'user',
-                ]:
+            "frequency_penalty",
+            "max_tokens",
+            "presence_penalty",
+            "temperature",
+            "top_p",
+            "user",
+        ]:
             val = getattr(self, attr)
-            if not val is None:
+            if val is not None:
                 payload[attr] = val
 
         return {
-                "identifier": self.identifier,
-                "endpoint": self.endpoint,
-                "api_key": self.api_key,
-                "payload": payload,
-                }
+            "identifier": self.identifier,
+            "endpoint": self.endpoint,
+            "api_key": self.api_key,
+            "payload": payload,
+        }
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 
 from .models import OpenAIChat
 
+
 def index(request):
     payload = OpenAIChat.objects.get(identifier="mock_1").serialize()
     return JsonResponse(payload)
-
