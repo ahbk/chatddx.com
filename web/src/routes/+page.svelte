9993201c3ed6e244313714cc793d97984cdb5ce3
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-01-26 11:43:59 +0100

    move to web

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<h1 class="text-4xl">
+  ChatDDx
+  <small class="text-sm">Differentialdiagnostiskt beslutsstöd för läkare</small>
+</h1>
+<section>
+  <label>
+    Patientunderlag
+    <textarea class="textarea textarea-bordered w-full"></textarea>
+  </label>
+  <button class="btn btn-primary">Skapa beslutstöd</button>
+</section>
+<section>
+  <div role="tablist" class="tabs tabs-lifted">
+    <a role="tab" class="tab">
+      Diagnoser att utesluta
+    </a>
+    <a role="tab" class="tab tab-active">
+      Undersökningar att planera
+    </a>
+    <a role="tab" class="tab">
+      Komplettera patientunderlag
+    </a>
+  </div>
+  <div class="">
+
+  </div>
+</section>
