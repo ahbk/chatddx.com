f3fba48f11ab02c39b663da20cbe37465c52d4e9
Alexander Holmbäck <alexander.holmback@gmail.com>
2024-01-29 20:47:15 +0100

    clean up flake.lock

--- flake.nix
+++ flake.nix
@@ -2,57 +2,105 @@
   description = "build chatddx";
 
   inputs = {
-    my-nixos.url = "github:ahbk/my-nixos";
-    nixpkgs.follows = "my-nixos/nixpkgs";
-    nixpkgs-stable.follows = "my-nixos/nixpkgs-stable";
+    nixpkgs.url = "github:ahbk/nixpkgs/nixos-unstable";
 
     poetry2nix = {
       url = "github:ahbk/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    buildNodeModules = {
+      url = "github:adisbladis/buildNodeModules";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, poetry2nix, ... }: 
+  outputs = { self, nixpkgs, poetry2nix, buildNodeModules, ... }: 
   with nixpkgs.lib;
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
     inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    inherit (buildNodeModules.lib.${system}) fetchNodeModules hooks;
+
     mkEnv = env: pkgs.writeText "env" (
       concatStringsSep "\n" (mapAttrsToList (k: v: "${k}=${v}") env)
       );
   in {
-    packages.${system}.django = rec {
-      bin = pkgs.substituteAll {
-        src = "${self}/backend/bin/chatddx.com";
-        dir = "bin";
-        isExecutable = true;
-        app = app.dependencyEnv;
-        inherit env static;
+    packages.${system} = rec {
+      default = pkgs.buildEnv {
+        name = "chatddx.com";
+        paths = [
+          svelte.app
+        ];
       };
 
-      env = mkEnv {
-        DEBUG = "true";
-        STATE_DIR = "/var/lib/chatddx.com";
-        HOST = "localhost";
-        DJANGO_SETTINGS_MODULE = "app.settings";
-      };
+      svelte.app = pkgs.stdenv.mkDerivation {
+        pname = "chatddx.com-svelte";
+        version = "0.1.0";
+        src = "${self}/client";
+        env = mkEnv {
+          PUBLIC_API="http://localhost:8000";
+          PUBLIC_API_SSR="http://localhost:8000";
+          ORIGIN="http://localhost:3000";
+        };
 
-      app = mkPoetryApplication {
-        projectDir = "${self}/backend";
-        groups = [];
-        checkGroups = [];
-      };
+        nodeModules = fetchNodeModules {
+          packageRoot = "${self}/client";
+        };
 
-      static = pkgs.stdenv.mkDerivation {
-        pname = "chatddx-static";
-        version = app.version;
-        src = "${self}/backend";
+        nativeBuildInputs = [
+          hooks.npmConfigHook
+          pkgs.nodejs_20
+          pkgs.npmHooks.npmBuildHook
+          pkgs.npmHooks.npmInstallHook
+        ];
+
         buildPhase = ''
-          export STATIC_ROOT=$out
-          export DJANGO_SETTINGS_MODULE=app.settings
-          ${app.dependencyEnv}/bin/django-admin collectstatic --no-input
+          set -a
+          source $env
+          set +a
+          npm run build
         '';
+
+        installPhase = ''
+          cp -r . $out
+        '';
+      };
+
+      django = rec {
+        bin = pkgs.substituteAll {
+          src = "${self}/backend/bin/chatddx.com";
+          dir = "bin";
+          isExecutable = true;
+          app = app.dependencyEnv;
+          inherit env static;
+        };
+
+        env = mkEnv {
+          DEBUG = "true";
+          STATE_DIR = "/var/lib/chatddx.com";
+          HOST = "localhost";
+          DJANGO_SETTINGS_MODULE = "app.settings";
+        };
+
+        app = mkPoetryApplication {
+          projectDir = "${self}/backend";
+          groups = [];
+          checkGroups = [];
+        };
+
+        static = pkgs.stdenv.mkDerivation {
+          pname = "chatddx-static";
+          version = app.version;
+          src = "${self}/backend";
+          buildPhase = ''
+            export STATIC_ROOT=$out
+            export DJANGO_SETTINGS_MODULE=app.settings
+            ${app.dependencyEnv}/bin/django-admin collectstatic --no-input
+          '';
+        };
       };
     };
   };
